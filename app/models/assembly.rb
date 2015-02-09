2a48a220bee44955d292cfb2b90a62e5bd68c235
chadwickdonald <chadwickbidwell@gmail.com>
2015-02-08 19:03:28 -0800

    fixing check_part_number

--- app/models/assembly.rb
+++ app/models/assembly.rb
@@ -1,10 +1,10 @@
 class Assembly < ActiveRecord::Base
 	has_and_belongs_to_many :parts,
-		before_add: check_part_number
+		before_add: :check_part_number
 
 		def check_part_number(part)
-			if part.part_number > 100
-				part.part_number += 200
+			if part.part_number.to_i > 100
+				part.part_number = 200
 			end
 		end
 end
